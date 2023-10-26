a89c601e43c2050b9b758f4d5a6fba137f9ff561
Arsemi337 <arturmilosz@onet.pl>
2023-10-26 15:57:32 +0200

    bert squad retrained

---
+++ qacore/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="qacore",
+    version="0.1.1",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9',
+)
