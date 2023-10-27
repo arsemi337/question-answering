254f1f24c6e1b30822e6dc62c7f17131b8826726
IrinUvis <irinuvis@gmail.com>
2023-10-28 00:35:10 +0200

    Most of the project setup done

---
+++ documentation/project_structure.md
@@ -0,0 +1,3 @@
+# Project structure
+
+Project structure description (T.B.D.)
---
+++ src/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name='question_answering',
+    version='0.1',
+    packages=setuptools.find_packages()
+)
