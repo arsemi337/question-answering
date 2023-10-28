3bf05c16fac81bdee2c06ef2f6f5eae6145565f3
IrinUvis <irinuvis@gmail.com>
2023-10-28 11:32:20 +0200

    Generative paths updated

--- src/question_answering/paths/generative_qa_paths.py
+++ src/question_answering/paths/generative_qa_paths.py
@@ -2,7 +2,7 @@ from pyprojroot import find_root, has_dir
 
 root = find_root(has_dir(".git"))
 
-generative_qa_dir = root / "generative_qa"
-training_checkpoints_dir = generative_qa_dir / "training_checkpoints"
-saved_models_dir = generative_qa_dir / "tf_models"
-hub_models_location = generative_qa_dir / "hub_models"
+generative_qa_dir = root / "generative-qa"
+training_checkpoints_dir = generative_qa_dir / "training-checkpoints"
+saved_models_dir = generative_qa_dir / "tf-models"
+hub_models_location = generative_qa_dir / "hub-models"
