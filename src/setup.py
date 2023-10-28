a50d20b0190ddb20874da561c8efd3753eeb3330
IrinUvis <irinuvis@gmail.com>
2023-10-28 13:17:33 +0200

    Installed black and isort

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,5 @@
 import setuptools
 
 setuptools.setup(
-    name='question_answering',
-    version='0.1',
-    packages=setuptools.find_packages()
+    name="question_answering", version="0.1", packages=setuptools.find_packages()
 )
