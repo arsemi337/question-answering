e7e95dd7e6c158885ec9110138f58622a5f06f4e
IrinUvis <irinuvis@gmail.com>
2023-10-30 17:59:55 +0100

    Saving and loading models + some minor changes

--- src/question_answering/constants/constants.py
+++ src/question_answering/constants/constants.py
@@ -1,1 +1,2 @@
 default_model_version = "1"
+checkpoint_filename_template = "cp-{epoch:02d}.ckpt"
--- src/question_answering/utils/core_qa_utils.py
+++ src/question_answering/utils/core_qa_utils.py
@@ -1,31 +1,38 @@
-import os
-
-import datasets
-import matplotlib.pyplot as plt
+from transformers import DataCollator
+from pathlib import Path
+from datasets import Dataset
 import pandas as pd
+import shutil
+import numpy as np
 import tensorflow as tf
 
 from question_answering.constants import constants
+from question_answering.paths import extractive_qa_paths
 
 
-def load_train_val_test_datasets(dataset_path):
+def load_train_val_test_datasets(dataset_path: Path):
     train = pd.read_csv(dataset_path / "train.csv").dropna()
     val = pd.read_csv(dataset_path / "val.csv").dropna()
     test = pd.read_csv(dataset_path / "test.csv").dropna()
     return train, val, test
 
 
-def convert_dataframes_to_datasets(dataframes: list):
+def convert_dataframes_to_datasets(dataframes: list[pd.DataFrame]):
     return tuple(
         [
-            datasets.Dataset.from_pandas(dataframe, preserve_index=False)
+            Dataset.from_pandas(dataframe, preserve_index=False)
             for dataframe in dataframes
         ]
     )
 
 
 def convert_to_tf_dataset(
-    hf_dataset, columns, label_cols, collator, batch_size, shuffle=False
+    hf_dataset: Dataset,
+    columns: list[str],
+    label_cols: list[str],
+    collator,
+    batch_size: int,
+    shuffle: bool = False,
 ):
     return hf_dataset.to_tf_dataset(
         columns=columns,
@@ -36,44 +43,30 @@ def convert_to_tf_dataset(
     )
 
 
-def plot_and_save_fig_from_history(
-    history,
-    attributes,
-    title,
-    y_label,
-    x_label,
-    legend_descriptors,
-    figure_dir,
-    figure_filename,
+def get_best_model_from_checkpoints(
+    trained_model: tf.keras.Model,
+    history: tf.keras.callbacks.History,
+    model_name: str,
+    metric: str = "val_loss",
+    remove_checkpoints: bool = True,
 ):
-    for attribute in attributes:
-        plt.plot(history.history[attribute])
-    plt.title(title)
-    plt.ylabel(y_label)
-    plt.xlabel(x_label)
-    plt.legend(legend_descriptors, loc="upper left")
-
-    _create_dirs_if_not_exists(figure_dir)
-
-    plt.savefig(figure_dir / figure_filename)
-    plt.show()
-
-
-def save_model(
-    model,
-    full_model_name,
-    models_dir,
-    default_model_version=constants.default_model_version,
-):
-    model.save(models_dir / full_model_name / default_model_version)
-
-
-def load_model(model_path, model_compile=True):
-    return tf.keras.models.load_model(
-        model_path / constants.default_model_version, compile=model_compile
+    best_model_index = np.argmin(history.history[metric]) + 1
+    best_model_checkpoints_path = (
+        extractive_qa_paths.training_checkpoints_dir / model_name
     )
+    best_model_weights_path = (
+        best_model_checkpoints_path
+        / constants.checkpoint_filename_template.format(epoch=best_model_index)
+    )
+    best_model = trained_model
+    best_model.load_weights(best_model_weights_path)
+
+    if remove_checkpoints:
+        shutil.rmtree(best_model_checkpoints_path)
+
+    return best_model
 
 
-def _create_dirs_if_not_exists(directory):
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
+def _create_dirs_if_not_exists(directory: Path):
+    if not directory.is_dir():
+        directory.mkdir()
