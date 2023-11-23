7d4745480e79cafd527de52cdd511cf9061c74ad
IrinUvis <irinuvis@gmail.com>
2023-11-23 21:56:40 +0100

    Extractive qa things

---
+++ src/question_answering/utils/extractive_qa/model_management.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from transformers import TFAutoModelForQuestionAnswering
+from question_answering.paths import extractive_qa_paths
+from question_answering.constants import constants
+from shutil import rmtree
+
+
+def load_best_model_from_checkpoints(
+        model: tf.keras.Model,
+        model_name: str,
+        epoch: int,
+        remove_checkpoints: bool
+):
+    model_checkpoints_path = extractive_qa_paths.training_checkpoints_dir / model_name
+    best_model_weights_path = model_checkpoints_path / constants.checkpoint_filename_template.format(epoch=epoch)
+
+    best_model = model
+    best_model.load_weights(best_model_weights_path)
+
+    if remove_checkpoints:
+        rmtree(model_checkpoints_path)
+
+    return best_model
+
+
+def save_model(
+        model: tf.keras.Model,
+        model_name: str
+):
+    model.save_weights(extractive_qa_paths.saved_models_dir / model_name / constants.saved_model_weights_name)
+
+
+def load_weights_into_model(
+        model: tf.keras.Model,
+        model_name: str
+) -> tf.keras.Model:
+    model.load_weights(extractive_qa_paths.saved_models_dir / model_name / constants.saved_model_weights_name)
+    return model
+
+
+def load_model(
+        model_checkpoint: str,
+        model_name: str
+) -> tf.keras.Model:
+    model = TFAutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
+    model.load_weights(extractive_qa_paths.saved_models_dir / model_name / constants.saved_model_weights_name)
+    return model
--- src/question_answering/utils/generative_qa_utils.py
+++ src/question_answering/utils/generative_qa_utils.py
@@ -12,29 +12,27 @@ from question_answering.paths import generative_qa_paths
 
 
 def save_model(
-        model: tf.keras.Model, model_name: str, weights_name: str = "model_weights"
+    model: tf.keras.Model, model_name: str, weights_name: str = "model_weights"
 ):
     model.save_weights(generative_qa_paths.saved_models_dir / model_name / weights_name)
 
 
 def load_weights_into_model(
-        model: tf.keras.Model, model_name: str, weights_name: str = "model_weights"
+    model: tf.keras.Model, model_name: str, weights_name: str = "model_weights"
 ) -> tf.keras.Model:
     model.load_weights(generative_qa_paths.saved_models_dir / model_name / weights_name)
     return model
 
 
 def load_model(
-        model_checkpoint: str, model_name: str, weights_name: str = "model_weights"
+    model_checkpoint: str, model_name: str, weights_name: str = "model_weights"
 ) -> tf.keras.Model:
     model = TFAutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
     model.load_weights(generative_qa_paths.saved_models_dir / model_name / weights_name)
     return model
 
 
-def generate_predictions(
-        model: tf.keras.Model, batch, max_length: int
-):
+def generate_predictions(model: tf.keras.Model, batch, max_length: int):
     return model.generate(
         input_ids=batch["input_ids"],
         attention_mask=batch["attention_mask"],
@@ -43,7 +41,10 @@ def generate_predictions(
 
 
 def get_dataset_dataframe_with_predictions(
-        model: tf.keras.Model, tokenizer: BartTokenizerFast, tf_dataset, max_length: int,
+    model: tf.keras.Model,
+    tokenizer: BartTokenizerFast,
+    tf_dataset,
+    max_length: int,
 ):
     predictions_list = []
     labels_list = []
@@ -51,7 +52,9 @@ def get_dataset_dataframe_with_predictions(
 
     for batch, labels in tqdm(tf_dataset):
         predictions = generate_predictions(model, batch, max_length)
-        decoded_predictions = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        decoded_predictions = tokenizer.batch_decode(
+            predictions, skip_special_tokens=True
+        )
         labels = labels.numpy()
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
@@ -60,38 +63,39 @@ def get_dataset_dataframe_with_predictions(
 
         predictions_list.extend(decoded_predictions)
         labels_list.extend(decoded_labels)
-        question_contexts_list.extend(tokenizer.batch_decode(batch['input_ids'], skip_special_tokens=True))
+        question_contexts_list.extend(
+            tokenizer.batch_decode(batch["input_ids"], skip_special_tokens=True)
+        )
 
-    return pd.DataFrame(list(zip(question_contexts_list, predictions_list, labels_list)),
-                        columns=['question_contexts', 'predictions', 'labels'])
+    return pd.DataFrame(
+        list(zip(question_contexts_list, predictions_list, labels_list)),
+        columns=["question_contexts", "predictions", "labels"],
+    )
 
 
-def split_questions_and_contexts_into_two_columns(
-        dataframe
-):
+def split_questions_and_contexts_into_two_columns(dataframe):
     questions = []
     contexts = []
 
-    if 'question_contexts' in dataframe:
+    if "question_contexts" in dataframe:
         for index, row in dataframe.iterrows():
-            questions.append(row['question_contexts'].split('?')[0] + '?')
-            contexts.append(row['question_contexts'].split('?')[1])
+            questions.append(row["question_contexts"].split("?")[0] + "?")
+            contexts.append(row["question_contexts"].split("?")[1])
 
         data = {
-            'questions': questions,
-            'contexts': contexts,
-            'labels': dataframe['labels'],
-            'predictions': dataframe['predictions']
+            "questions": questions,
+            "contexts": contexts,
+            "labels": dataframe["labels"],
+            "predictions": dataframe["predictions"],
         }
         return pd.DataFrame(data)
     else:
         return pd.DataFrame().empty
 
-def get_metrics(
-        dataset_dataframe_with_predictions
-):
-    predictions_list = dataset_dataframe_with_predictions['predictions']
-    labels_list = dataset_dataframe_with_predictions['labels']
+
+def get_metrics(dataset_dataframe_with_predictions):
+    predictions_list = dataset_dataframe_with_predictions["predictions"]
+    labels_list = dataset_dataframe_with_predictions["labels"]
 
     bleu_metric = evaluate.load("bleu")
     rouge_metric = evaluate.load("rouge")
@@ -99,10 +103,20 @@ def get_metrics(
     bertscore_metric = evaluate.load("bertscore")
     sacrebleu_metric = evaluate.load("sacrebleu")
 
-    bleu_result = bleu_metric.compute(predictions=predictions_list, references=labels_list)
-    rogue_result = rouge_metric.compute(predictions=predictions_list, references=labels_list)
-    meteor_result = meteor_metric.compute(predictions=predictions_list, references=labels_list)
-    bertscore_result = bertscore_metric.compute(predictions=predictions_list, references=labels_list, lang='en')
-    sacrebleu_result = sacrebleu_metric.compute(predictions=predictions_list, references=labels_list)
+    bleu_result = bleu_metric.compute(
+        predictions=predictions_list, references=labels_list
+    )
+    rogue_result = rouge_metric.compute(
+        predictions=predictions_list, references=labels_list
+    )
+    meteor_result = meteor_metric.compute(
+        predictions=predictions_list, references=labels_list
+    )
+    bertscore_result = bertscore_metric.compute(
+        predictions=predictions_list, references=labels_list, lang="en"
+    )
+    sacrebleu_result = sacrebleu_metric.compute(
+        predictions=predictions_list, references=labels_list
+    )
 
     return bleu_result, rogue_result, meteor_result, bertscore_result, sacrebleu_result
