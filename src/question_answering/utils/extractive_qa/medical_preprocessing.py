7460c0735c5e88d26f235d778b56739428c000a7
IrinUvis <irinuvis@gmail.com>
2023-12-06 15:51:25 +0100

    Stride model done, but requires training

--- src/question_answering/utils/extractive_qa/medical_preprocessing.py
+++ src/question_answering/utils/extractive_qa/medical_preprocessing.py
@@ -122,3 +122,73 @@ def preprocess_medical_test_dataset(
     return dataset.map(
         preprocess_samples, batched=batched, remove_columns=remove_columns
     )
+
+
+def preprocess_medical_test_dataset_no_stride(
+        dataset: Dataset,
+        tokenizer,
+        max_length: int,
+        batched: bool = True,
+        remove_columns: list[str] = None,
+):
+    def preprocess_samples(samples):
+        questions = [q.strip() for q in samples["question"]]
+        contexts = [c.strip() for c in samples["context"]]
+
+        inputs = tokenizer(
+            questions,
+            contexts,
+            max_length=max_length,
+            padding="max_length",
+            return_offsets_mapping=True,
+        )
+
+        offset_mapping = inputs.pop("offset_mapping")
+        answer_starts = samples["answer_start"]
+        answer_texts = samples["answer_text"]
+        start_positions = []
+        end_positions = []
+        example_ids = []
+        new_offset_mapping = []
+
+        for i, offset in enumerate(offset_mapping):
+            example_id = samples["id"][i]
+            answer_start = answer_starts[i][0]
+            answer_text = answer_texts[i][0]
+            start_char = answer_start
+            end_char = start_char + len(answer_text)
+            sequence_ids = inputs.sequence_ids(i)
+
+            # Find the start and end of the context
+            idx = 0
+            while sequence_ids[idx] != 1:
+                idx += 1
+            context_start = idx
+            while sequence_ids[idx] == 1:
+                idx += 1
+            context_end = idx - 1
+
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+            example_ids.append(example_id)
+            new_offset_mapping.append(
+                [o if sequence_ids[k] == 1 else None for k, o in enumerate(offset)]
+            )
+
+        inputs["start_positions"] = start_positions
+        inputs["end_positions"] = end_positions
+        inputs["example_id"] = example_ids
+        inputs["offset_mapping"] = new_offset_mapping
+        return inputs
+
+    return dataset.map(
+        preprocess_samples, batched=batched, remove_columns=remove_columns
+    )
